8810f8feb08bd295669c2b54619ab2374d3f0b9d
AliChoopani08 <chopaniali373@gmail.com>
2026-05-18 08:51:43 +0330

    resolve product search problem

--- src/main/java/com/Ali/Store/App/dto/product/request/SearchProductRequest.java
+++ src/main/java/com/Ali/Store/App/dto/product/request/SearchProductRequest.java
@@ -1,11 +1,8 @@
 package com.Ali.Store.App.dto.product.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
-
 import java.math.BigDecimal;
 
 @Builder
-public record SearchProductRequest(@JsonProperty("Category")String category, @JsonProperty("Name")String name
-        , @JsonProperty("Max productPrice") BigDecimal maxPrice,
-                                   @JsonProperty("Min productPrice")BigDecimal minPrice, Boolean status) {}
+public record SearchProductRequest(String category, String name
+        , BigDecimal maxPrice, BigDecimal minPrice, Boolean status) {}
